13d93f4a08fe457a355c76ba922298e2886ce806
venomiele <atrandafir35@yahoo.com>
2022-01-10 13:28:45 -0800

    added toast logic training

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 let navbar = document.querySelector(".nav");
 let btn = document.querySelector(".btn");
+let toast = document.querySelector(".toast-btn");
+let toastCon = document.querySelector(".toast-container");
 
 
 btn.addEventListener("click", () => {
     navbar.classList.toggle("active");
     btn.classList.toggle("active-btn");
 })
+
+//Toast
+
+toast.addEventListener("click", () => {
+createNotification();
+})
+
+function createNotification() {
+    let notif = document.createElement("div");
+    notif.classList.add("toasted");
+    notif.innerText = "This is a notification !";
+    toastCon.appendChild(notif);
+
+    setTimeout(() => {
+notif.remove();
+    },3000)
+}
